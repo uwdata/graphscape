bef829dc8178f757506bc9ec9015bf9dddb45f33
Younghoon Kim <yhoonkim01@gmail.com>
2016-02-08 18:15:34 -0800

    Added the progress indicator for user. (#10)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,14 @@ class HomeController < ApplicationController
     # offset = rand(1323440)
     # offset = rand(24952)
     # offset = rand(388)
-    offset = rand(ENV["TRIPLET_COUNT"].to_i) + 1
+    @offset = rand(ENV["TRIPLET_COUNT"].to_i) + 1
     # offset = 89335
-    p offset
-    @triplet = Triplet.find_by_id(offset) || Triplet.find_by_id_in_csv(offset)
+    @user_covered = current_user.human_answers.where("created_at >= ?", (Time.now-24.hours)).count
+    @daily_goal = 2
+
+    @total = ENV["TRIPLET_COUNT"].to_i
+    @covered = HumanAnswer.pluck(:triplet_id).uniq.count
+    @triplet = Triplet.find_by_id(@offset) || Triplet.find_by_id_in_csv(@offset)
     # @triplet = Triplet.order("RANDOM()").first
 
     @human_answer = HumanAnswer.new()
