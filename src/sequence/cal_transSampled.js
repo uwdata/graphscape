d9ecddb875e9cf6b916bfd4e8b3acf2511bfde77
Younghoon Kim <yhoonkim01@gmail.com>
2016-03-14 17:43:30 -0700

    Added another set of sample visualizations (corresponding to A DEEPER UNDERSTANDING OF SEQUENCE IN NARRATIVE VISUALIZATION's work).

--- src/sequence/cal_transSampled.js
+++ src/sequence/cal_transSampled.js
@@ -4,7 +4,14 @@ var fs = require('fs');
 // var cp = require('./../../bower_components/viscompass')
 var cp = require('./../../lib/compass.js');
 var cpTrans = cp.trans;
+
+// Younghoon's Polestar bookmars
 var specs = JSON.parse(fs.readFileSync('./../../data/sampled_specs.json','utf8'));
+
+//  visualizations corresponding to Jessica's paper
+// var specs = JSON.parse(fs.readFileSync('./../../data/sampled_specs_prev_paper.json','utf8'));
+
+
 var ruleSet = JSON.parse(fs.readFileSync('./../../ruleSet.json','utf8'));
 var transitionSets = [];
 
