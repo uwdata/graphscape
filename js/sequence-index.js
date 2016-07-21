e1c1a95d32f178b9582f1995ca73fb85e056d43f
Younghoon Kim <yhoonkim01@gmail.com>
2016-07-21 11:12:41 -0700

    Add controls

--- js/sequence-index.js
+++ js/sequence-index.js
@@ -1,5 +1,7 @@
 $(document).on('ready page:load', function () {
   var ruleSets;  
+  var results;
+
   $.ajax({
     url: "data/ruleSet.json",
     dataType: 'json',
@@ -13,9 +15,72 @@ $(document).on('ready page:load', function () {
   var worker = new Worker('js/sequence-worker.js');
   
 
+  $('#toggle-show-all').on('click',function(e){
+    if (!$(this).hasClass('active')) {
+      $('#sorted-result .not.optimum').removeClass('hidden');  
+      $(this).addClass('active');
+      $(this).text('Show Optimum Only');
+    } else {
+      $('#sorted-result .not.optimum').addClass('hidden');
+      $(this).removeClass('active');
+      $(this).text('Show All');
+    }
+  });
+
+  $('#toggle-pattern-score').on('click',function(e){
+    if ($(this).hasClass('active')) {
+      results = results.sort(function(a,b){
+        if (a.distance > b.distance) {
+          return 1;
+        }
+        if (a.distance < b.distance) {
+          return -1;
+        }
+        return 0;
+      });
+      var minDistance = results[0].distance;
+      for (var i = 0; i < results.length; i++) {
+        if(results[i].distance === minDistance){
+          results[i].isOptimum = true;
+        }
+        else { 
+          results[i].isOptimum = false; 
+        }
+      }
+      listButtons(results)
+      $(this).removeClass('active');
+      $(this).text('On Pattern Score');
+      
+    } else {
+      results = results.sort(function(a,b){
+        if (a.globalScore < b.globalScore) {
+          return 1;
+        }
+        if (a.globalScore > b.globalScore) {
+          return -1;
+        }
+        return 0;
+      });
+      var maxGlobalScore = results[0].globalScore;
+      for (var i = 0; i < results.length; i++) {
+        if(results[i].globalScore === maxGlobalScore){
+          results[i].isOptimum = true;
+        }
+        else { 
+          results[i].isOptimum = false;
+        }
+      }
+
+      listButtons(results);
+      $(this).addClass('active');
+      $(this).text('Off Pattern Score');
+    }
+    
+  });
+
 
   $('#sort').on('click', function(e){
-    var results;
+    
     
     var specs = JSON.parse($('#specs').val());
     var fixFirst = $('#fixfirst').is(':checked');
@@ -27,30 +92,42 @@ $(document).on('ready page:load', function () {
         setTimeout(function(){ 
           $('#current-status').hide(500);  
         }, 1);
-        
         results = e.data;
-        $('#sorted-result').children().remove();
-        for (var i = 0; i < results.length; i++) {
-          var link = $('<button href="#" data-id="'+i+'"></button>')
-                      .html(results[i].sequence.join(',') + ' | ' + Math.round(results[i].globalScore*100)/100 );
-          link.addClass('result btn btn-default btn-xs');
-          link.data('result', results[i]);
-          
-          $('#sorted-result').append(link);
-
-          
-        };
-
-        $('.result').on('click', function(){
-          drawingByOrder(results[$(this).data('id')]);
-          console.log(results[$(this).data('id')]);
-        });
+        listButtons(results);
+        
       }
       worker.postMessage({specs: specs, ruleSets: ruleSets, options: {"fixFirst": fixFirst}}); // Start the worker.
     });
   
   })
+  function listButtons(results){
 
+    $('#sorted-result .optimum').children().remove();
+    $('#sorted-result .not.optimum').children().remove();
+    $('#control-result').removeClass("hidden");
+
+    for (var i = 0; i < results.length; i++) {
+      var link = $('<button href="#" data-id="'+i+'"></button>')
+                  .html(results[i].sequence.join(',') + ' | ' + Math.round(results[i].globalScore*100)/100 )
+                  .addClass('result btn btn-xs')
+                  .data('result', results[i]);
+      
+      if (results[i].isOptimum) { 
+        link.addClass('btn-primary');
+        $('#sorted-result .optimum').not('.not').append(link);
+      }
+      else {
+        link.addClass('btn-default');
+        $('#sorted-result .not.optimum').append(link);
+      };
+      
+    };
+
+    $('.result').on('click', function(){
+      drawingByOrder(results[$(this).data('id')]);
+      console.log(results[$(this).data('id')]);
+    });
+  }
 
 
 
--- js/sequence-worker.js
+++ js/sequence-worker.js
@@ -2,7 +2,7 @@ self.onmessage = function(e) {
   importScripts('../js/compass.min.js');
   importScripts('../js/serialize-web.js');
   serializer.serialize(e.data.specs, e.data.ruleSets, e.data.options, function(serializedSpecs){ 
-    self.postMessage(serializedSpecs.all);
+    self.postMessage(serializedSpecs);
   });
   
 };
